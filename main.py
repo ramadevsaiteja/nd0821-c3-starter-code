2777a50534057298d41a6711b9b794a9f797138b
ramadevsaiteja <raminenisaiteja123@gmail.com>
2022-01-30 14:54:31 +0530

    Add Aptfile

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from train.ml.data import process_data
 from train.ml.model import inference
 import os
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 app = FastAPI()
 
 
